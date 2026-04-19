752b884883e4133283daf7550e13a741e5e367d4
deividation <deividas@theverylab.com>
2026-04-19 19:52:44 +0300

    chore: redeploy with new API key

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-// v1776616009083
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+// v1776617564981
